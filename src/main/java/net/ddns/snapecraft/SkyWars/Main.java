f03ae563c0631d8c0d7692f9517da7f87dd9764a
tallerik <erik.klasse@gmail.com>
2018-12-05 18:48:46 +0100

    Initial Commit

---
+++ src/main/java/net/ddns/snapecraft/SkyWars/Main.java
@@ -0,0 +1,25 @@
+package net.ddns.snapecraft.SkyWars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        init();
+    }
+    @Override
+    public void onDisable() {
+
+    }
+
+
+    private void init() {
+
+        //Bukkit.getPluginManager().registerEvent();
+        //Bukkit.getPluginCommand("").setExecutor();
+
+    }
+
+}
